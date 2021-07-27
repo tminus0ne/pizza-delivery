ab2165a91e8697f22eeca13fc766fa12e7e4e56a
Greg-One <sgmdoc@yandex.ru>
2021-07-28 00:14:57 +0300

    add popul open & close functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from './components/Card/Card';
+import { Card } from './components/Card';
 import { Categories } from './components/Categories';
 import { Header } from './components/Header';
 import { Popup } from './components/Popup';
--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -3,13 +3,37 @@ import React from 'react';
 import styles from './Popup.module.scss';
 
 export const Popup = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const sortRef = React.useRef();
+
+  const togglePopupVisibility = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOutsideClick = (evt) => {
+    if (!evt.path.includes(sortRef.current)) {
+      setIsOpen(false);
+    }
+  };
+
+  React.useEffect(() => {
+    document.body.addEventListener('click', handleOutsideClick);
+  }, []);
+
   return (
-    <div className={styles.popup}>
+    <div ref={sortRef} className={styles.popup}>
       <button>
         <img src="/img/popup-arrow.svg" alt="" />
       </button>
       <p>Sort by:</p>
-      <span>popularity</span>
+      <span onClick={togglePopupVisibility}>popularity</span>
+      {isOpen && (
+        <ul className={styles.list}>
+          <li className={styles.active}>popularity</li>
+          <li>price</li>
+          <li>name</li>
+        </ul>
+      )}
     </div>
   );
 };
