e59760d4ba76fe1791bf923c2d0c40683a8c4092
Greg-One <sgmdoc@yandex.ru>
2021-08-11 22:32:08 +0500

    add async fetchPizzas function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import axios from 'axios';
 import { useDispatch } from 'react-redux';
 
 import { Header } from './components/Header';
 import { Cart } from './pages/Cart';
 import { Home } from './pages/Home';
-import { setPizzas } from './redux/actions/pizzas';
+import { fetchPizzas } from './redux/actions/pizzas';
 
 export function App() {
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      dispatch(setPizzas(data.pizzas));
-    });
+    dispatch(fetchPizzas());
   }, []);
 
   return (
--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -1,4 +1,12 @@
+import axios from 'axios';
+
 export const setPizzas = (items) => ({
   type: 'SET_PIZZAS',
   payload: items,
 });
+
+export const fetchPizzas = () => (dispatch) => {
+  axios.get('http://localhost:3001/pizzas').then(({ data }) => {
+    dispatch(setPizzas(data));
+  });
+};
